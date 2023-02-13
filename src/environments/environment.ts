ae20aa025c53d4ad859017696a0bfe564f60d248
roshanaale9818 <itachiuchhia9818@gmail.com>
2023-02-13 22:30:08 +1100

    firebase host integrated in new project and app host configs added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,15 @@
 export const envrionment ={
   // apiUrl
-  apiUrl:"http://localhost:8080/api"
+  // apiUrl:"http://localhost:8080/api",
+  apiUrl:"https://angulartodoapp-376402.de.r.appspot.com/api",
+  googleKey:"AIzaSyAxHTYq4hO4re9eXKoblMFmhhh_cPcWVNU",
+   firebaseConfig : {
+    apiKey: "AIzaSyDlFHIP1tvo4dYPI4sKu3lDrT_gv3ln0zQ",
+    authDomain: "clientpanelprod-edb0d.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-edb0d.firebaseio.com",
+    projectId: "clientpanelprod-edb0d",
+    storageBucket: "clientpanelprod-edb0d.appspot.com",
+    messagingSenderId: "322677051827",
+    appId: "1:322677051827:web:1026d2afaeec0a60"
+  }
 }
